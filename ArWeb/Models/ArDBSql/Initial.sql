aebaaaccafd4a924cc7405152b2670da316b8757
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2020-10-20 16:04:23 +0800

    Database Initialize
    +Added ARDB-2020 database file

--- ArWeb/Models/ArDBSql/Initial.sql
+++ ArWeb/Models/ArDBSql/Initial.sql
@@ -4,9 +4,9 @@
 insert into ArAccStatus([Status]) values
 ('Active'),('Inactive'),('OnHold');
 
-insert into ArPaymentType([Type]) values
+insert into ArPaymentTypes([Type]) values
 ('Cash'),('Check'),('Bank'),('PO'),('Others');
 
-insert into ArActionItem([Action],[Remarks],[SortNo]) values
+insert into ArActionItems([Action],[Remarks],[SortNo]) values
 ('New Bill','',1),('Bill Sent','',2),('Bill Received','',3),
 ('1st Reminder','',4),('2nd Reminder','',5),('Paid','',6);
