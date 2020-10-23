c4fe00fcac2b46e4f9522a6596393f55c32c763d
Abel Salvatierra <assalvatierra@gmail.com>
2020-10-23 15:07:12 +0800

    Ar.Rpt.list and summary sql

---
+++ ArModels/Models/ArDBSql/Rpt.AR.List.byCompany.sql
@@ -0,0 +1,16 @@
+﻿select 
+	a.InvoiceId,
+	a.DtInvoice,
+	b.Name,
+	b.Company,
+	a.Description,
+	a.DtDue, 
+	a.Amount, 
+	a.Remarks,
+	c.Status
+from ArTransactions a
+left outer join ArAccounts b on a.ArAccountId = b.Id
+left outer join ArTransStatus c on a.ArTransStatusId = c.Id
+
+order by a.ArAccountId, a.DtInvoice
+;
---
+++ ArModels/Models/ArDBSql/Rpt.AR.Summary.byCompany.sql
@@ -0,0 +1,20 @@
+﻿select 
+	b.Name,
+	b.Company, 
+	x.InvCnt,
+	x.AcntAmount
+from 
+(
+select 
+	max(a.ArAccountId) AccntId,
+	count(a.InvoiceId) InvCnt,
+	count(a.Amount) AcntAmount,
+	count(a.ArTransStatusId) ArStatus
+from 
+	ArTransactions a
+group by a.ArAccountId
+) x
+left outer join ArAccounts b on x.AccntId = b.Id
+
+order by x.AccntId
+;
